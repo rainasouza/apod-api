6709686ae2bc73e44c65cf043c1e6dc1f270fdbb
Raína Souza <raiina.py@gmail.com>
2024-10-20 12:52:51 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import NavBar from './components/Navbar/NavBar';
+import './App.css';
+import Home from './pages/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <br></br>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
